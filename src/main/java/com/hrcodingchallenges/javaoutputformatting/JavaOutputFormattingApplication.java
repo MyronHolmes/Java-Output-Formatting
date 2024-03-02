16f950ad3033008819a76cb2e9f4e132ea674482
Myron Holmes <1dequanh@gmail.com>
2024-03-01 21:47:26 -0500

    Completed

---
+++ src/main/java/com/hrcodingchallenges/javaoutputformatting/JavaOutputFormattingApplication.java
@@ -0,0 +1,12 @@
+package com.hrcodingchallenges.javaoutputformatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOutputFormattingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOutputFormattingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hrcodingchallenges/javaoutputformatting/JavaOutputFormattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrcodingchallenges.javaoutputformatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOutputFormattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
